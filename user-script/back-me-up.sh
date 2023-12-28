c79bb51ea806284f813f17c8f65ea5187d02a730
t.me/sasha_sup <a.suprun1312@gmail.com>
2023-12-28 22:50:52 +0400

    fix bup with same name

--- user-script/back-me-up.sh
+++ user-script/back-me-up.sh
@@ -4,22 +4,21 @@ BASE_DIR="$HOME/Code/studies/arch-i3wm-bup"
 CONF_DIR="$HOME/.config"
 DATE=$(date +"%Y-%m-%d")
 
-
-declare -a CONFIG_FILES=(
-    "$CONF_DIR/i3status/config"
-    "$CONF_DIR/i3/config"
-    "$CONF_DIR/dolphinrc"
-    "$CONF_DIR/terminator/config"
-    "$HOME/.bashrc"
-    "$HOME/.pgpass"
-    "$CONF_DIR/ksnip/ksnip.conf"
-    "/etc/tlp.conf"
-    "/etc/thinkfan.conf"
+declare -A CONFIG_FILES=(
+    ["$CONF_DIR/i3status/config"]="i3status_config_backup"
+    ["$CONF_DIR/i3/config"]="i3_config_backup"
+    ["$CONF_DIR/dolphinrc"]="dolphinrc_backup"
+    ["$CONF_DIR/terminator/config"]="terminator_config_backup"
+    ["$HOME/.bashrc"]="bashrc_backup"
+    ["$HOME/.pgpass"]="pgpass_backup"
+    ["$CONF_DIR/ksnip/ksnip.conf"]="ksnip_config_backup"
+    ["/etc/tlp.conf"]="tlp_conf_backup"
+    ["/etc/thinkfan.conf"]="thinkfan_conf_backup"
 )
 
-for config_file in "${CONFIG_FILES[@]}"; do
+for config_file in "${!CONFIG_FILES[@]}"; do
     config_name=$(basename "$config_file")
-    backup_name="$config_name"_"$DATE"
+    backup_name="${CONFIG_FILES[$config_file]}_$DATE"
     cp "$config_file" "$BASE_DIR/$backup_name"
 done
 

90c0dab897d65d2eebec58dd43a8ef193bedf5d5
t.me/sasha_sup <a.suprun1312@gmail.com>
2024-07-05 23:57:13 +0300

    fix/mem2much

--- user-script/pssss.sh
+++ user-script/pssss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CPU_TRSH=60
-MEM_TRSH=70
+MEM_TRSH=30
 
 if_cpu_hot() {
     CPU_TEMP=$(sensors | grep 'Package id 0' | awk '{print $4}' | tr -dc '0-9.')
@@ -12,10 +12,10 @@ if_cpu_hot() {
 }
 
 if_mem_2much() {
-    MEM_CHECK=$(free -m | awk 'NR==2 {print ($6/($3+$4))*100}')
+    MEM_CHECK=$(free -m | awk 'NR==2 {print ($7/$2)*100}')
     PS_MEM=$(ps -eo pid,cmd,%mem --sort=-%mem | head -6)
 
-    if [ $(echo "$MEM_CHECK > $MEM_TRSH" | bc) -eq 1 ]; then
+    if [ $(echo "$MEM_CHECK < $MEM_TRSH" | bc) -eq 1 ]; then
         dunstify "🚨 Ps, top 5 MEM processes 🚨" "$PS_MEM"
     fi
 }

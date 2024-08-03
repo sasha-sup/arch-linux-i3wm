c85fc4d5848503da430a1c4a340a45339f0abf5a
t.me/sasha_sup <a.suprun1312@gmail.com>
2024-08-03 21:02:23 +0300

    fix/hdmi

--- user-script/hdmi-output.sh
+++ user-script/hdmi-output.sh
@@ -7,9 +7,14 @@ if echo "$output" | grep -q "DP-2 connected"; then
     connected="DP-2"
 elif echo "$output" | grep -q "HDMI-1 connected"; then
     connected="HDMI-1"
+else
+    connected=""
 fi
 
-
 if [ -n "$connected" ]; then
-    xrandr --output "$connected" --mode "$resolution" 
+    if echo "$output" | grep -q "$connected connected [0-9]"; then
+        xrandr --output "$connected" --off
+    else
+        xrandr --output "$connected" --mode "$resolution"
+    fi
 fi
